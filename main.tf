3a71b748ad0debc7e1c020e701ef84294ae2bb8f
DorianMikolajczyk <91832798+DorianMikolajczyk@users.noreply.github.com>
2023-02-18 19:23:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  region = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "example-instance"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello, World!'"
+    ]
+  }
 }
