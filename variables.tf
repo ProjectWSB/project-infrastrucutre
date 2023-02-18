64f8bf7b33a083668810437c97e99f2cdc812e69
DorianMikolajczyk <91832798+DorianMikolajczyk@users.noreply.github.com>
2023-02-18 19:19:11 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,16 @@
+variable "region" {
+  description = "The AWS region where the EC2 instance will be created"
+  type        = string
+  default     = "us-east-1"
+}
 
+variable "instance_type" {
+  description = "The type of EC2 instance to launch"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0"
